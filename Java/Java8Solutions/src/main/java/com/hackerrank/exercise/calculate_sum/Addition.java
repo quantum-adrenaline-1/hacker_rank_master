fe6e7c1ed41f0cdf56f271b0da28c107c892c59f
quantum-adrenaline-1 <62058166+quantum-adrenaline-1@users.noreply.github.com>
2020-11-12 03:26:38 +0530

    Updated the function name to addition in Functional Interface Addition.java

--- Java/Java8Solutions/src/main/java/com/hackerrank/exercise/calculate_sum/Addition.java
+++ Java/Java8Solutions/src/main/java/com/hackerrank/exercise/calculate_sum/Addition.java
@@ -3,6 +3,6 @@ package com.hackerrank.exercise.calculate_sum;
 @FunctionalInterface
 public interface Addition {
 
-	int add(int p, int q);
+	int addition(int p, int q);
 	
 }
---
+++ Java/Java8Solutions/src/main/java/com/hackerrank/exercise/calculate_sum/CalculateSum.java
@@ -0,0 +1,5 @@
+package com.hackerrank.exercise.calculate_sum;
+
+public class CalculateSum {
+
+}

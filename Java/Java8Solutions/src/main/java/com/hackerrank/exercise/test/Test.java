303b57f2f2d06fb5e247b12808cd1e44ed60afe6
quantum-adrenaline-1 <62058166+quantum-adrenaline-1@users.noreply.github.com>
2020-11-12 16:10:07 +0530

    Created Test class 'Test.java' in 'test' package for test implementations

---
+++ Java/Java8Solutions/src/main/java/com/hackerrank/exercise/test/Test.java
@@ -0,0 +1,11 @@
+package com.hackerrank.exercise.test;
+
+import static java.lang.System.out;
+
+public class Test {
+
+	public static void main(String[] args) {
+		out.println("Hello, world!");
+	}
+
+}
